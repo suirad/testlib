40914989e9a56d28cbc73ced6403baec3d74ce9f
Suirad <suirad@users.noreply.github.com>
2019-07-26 23:29:57 -0500

    Update comments

--- src/testlib.nim
+++ src/testlib.nim
@@ -7,12 +7,16 @@ const
 # link to an archive
 {.passL: "-l:testlib.a".}
 
+# import a type by name from a header; still need to expose underlying type fields
 type
   Thinger* {.importc: "Thinger", header: test_header.} = object
     one*: int
     two*: int
 
+# import the function from the .h file
 proc doTheThing*(): int {.header: test_header.}
-proc sumThinger*(t: ptr Thinger): int {.header: test_header.}
+
+# import the function by symbol name
+proc sumThinger*(t: ptr Thinger): int {.importc: "sumThinger".}
 
 
